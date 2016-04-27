6c7369a651b55ff0169d186a87afe524636442c1
Ryan Graham <nashgryanb@gmail.com>
2016-04-27 08:23:03 -0600

    saving partial on easy 18

--- codeeval-easy-18.js
+++ codeeval-easy-18.js
@@ -1,17 +1,28 @@
+// https://www.codeeval.com/open_challenges/18/
+
+//Given numbers x and n, where n is a power of 2, print out the smallest multiple of n which is greater than or equal to x. Do not use division or modulo operator.
+
+
+// this is coming back as "Partially" but can't see why. Score 90. 
+
 var fs  = require("fs");
 fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
     if (line !== "") {
   		var temp = line.split(",");
-  		var x = parseInt(temp[0]); 
-  		var n = parseInt(temp[1]);
+  		var compareNum = parseInt(temp[0]); 
+  		var toMult = parseInt(temp[1]);
   		var stdout = 1; 
   		var i = 2; 
 
-  		while (stdout <= x) {
-  			stdout = n * i; 
+  		while (stdout <= compareNum) {
+  			stdout = toMult * i; 
   			i++; 
   		}
-  		console.log(stdout + "\n");
+  		console.log(stdout);
 
     }
 });
+
+
+
+
